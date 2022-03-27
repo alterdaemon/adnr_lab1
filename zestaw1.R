81b38b7964d1baeaff76e0ef39297c37bc23a3be
unknown <4732829+alterdaemon@users.noreply.github.com>
2022-03-27 17:57:52 +0200

    pierwszy plik

---
+++ zestaw1.R
@@ -0,0 +1,28 @@
+#1. Wczytaj plik autaSmall.csv i wypisz pierwsze 5 wierszy
+
+
+
+
+
+#2. Pobierz dane pogodowe z REST API
+
+
+
+
+#3.Napisz funckję zapisującą porcjami danych plik csv do tabeli  w SQLite
+
+
+readToBase<-function(filepath,dbConn,tablename,size,sep=",",header=TRUE,delete=TRUE){
+  #...
+}
+
+
+
+
+#4.Napisz funkcję znajdującą tydzień obserwacji z największą średnią ceną ofert korzystając z zapytania SQL.
+
+
+
+
+
+#5. Podobnie jak w poprzednim zadaniu napisz funkcję znajdującą tydzień obserwacji z największą średnią ceną ofert  tym razem wykorzystując REST api.
